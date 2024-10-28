0bbd5b6583e89e5b64c27dea15fb5d5aa97af128
COMRAD <mirolego1@gmail.com>
2024-10-28 13:04:28 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// Define two constant numbers
+const num1 = 10;
+const num2 = 5;
+
+// Perform calculations
+const sum = num1 + num2;
+const diff = num1 - num2;
+const prod = num1 * num2;
+
+// Output the results
+console.log('Addition:' + num1 + '+' + num2 + '=' + sum);
+console.log('Subtraction:' + num1 + '-' + num2 + '=' + diff);
+console.log('Product:' + num1 + 'x' + num2 + '=' + prod);
